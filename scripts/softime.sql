7037732411616ec2978e61bafe2c1bdfd2a53c94
GermanRz <germanramirez@outlook.com>
2022-08-23 11:31:50 -0500

    adicion del script de la BD con la tabla de usuarios

---
+++ scripts/softime.sql
@@ -0,0 +1,444 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 18-08-2022 a las 06:05:25
+-- Versión del servidor: 10.4.21-MariaDB
+-- Versión de PHP: 8.0.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `softime`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categorias`
+--
+
+CREATE TABLE `categorias` (
+  `id_categoria` int(11) NOT NULL,
+  `nombre_categoria` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `compra`
+--
+
+CREATE TABLE `compra` (
+  `id_compra` int(10) NOT NULL,
+  `id_proveedor` int(11) NOT NULL,
+  `id_producto` int(11) NOT NULL,
+  `fecha` date NOT NULL,
+  `precio_unitario` int(20) NOT NULL,
+  `cantidad` int(10) NOT NULL,
+  `precio_total` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `distribucion`
+--
+
+CREATE TABLE `distribucion` (
+  `id_distribucion` int(20) NOT NULL,
+  `id_venta` int(10) NOT NULL,
+  `id_transporte` varchar(50) NOT NULL,
+  `numero_factura` int(10) NOT NULL,
+  `estado` enum('Preparando en Bodega','En distribucion') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `entrega`
+--
+
+CREATE TABLE `entrega` (
+  `id_entrega` int(10) NOT NULL,
+  `id_compra` int(10) NOT NULL,
+  `transporte` varchar(50) NOT NULL,
+  `placa` varchar(6) NOT NULL,
+  `chofer_nombre` varchar(50) DEFAULT NULL,
+  `numero_factura` int(10) NOT NULL,
+  `estado` enum('En Bodega','En distribucion') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `facturas`
+--
+
+CREATE TABLE `facturas` (
+  `id_factura` int(10) NOT NULL,
+  `fecha` date NOT NULL,
+  `id_venta` int(10) NOT NULL,
+  `id_compra` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pedido`
+--
+
+CREATE TABLE `pedido` (
+  `id_pedido` int(20) NOT NULL,
+  `id_cliente` int(10) NOT NULL,
+  `id_proveedor` int(11) NOT NULL,
+  `precio_unitario` int(20) DEFAULT NULL,
+  `cantidad` int(20) NOT NULL,
+  `precio_total` int(20) NOT NULL,
+  `fecha_pedido` date NOT NULL,
+  `fecha_entrega` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producto`
+--
+
+CREATE TABLE `producto` (
+  `id_producto` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `descripcion` varchar(50) DEFAULT NULL,
+  `categoria` int(11) NOT NULL,
+  `cantidad` int(8) NOT NULL,
+  `precio_unitario` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `proveedor`
+--
+
+CREATE TABLE `proveedor` (
+  `id_proveedor` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `nit` varchar(11) NOT NULL,
+  `direccion` varchar(50) DEFAULT NULL,
+  `telefono` int(11) NOT NULL,
+  `correo` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `registro_pedido`
+--
+
+CREATE TABLE `registro_pedido` (
+  `id_registro_pedido` int(11) NOT NULL,
+  `id_pedido` int(20) NOT NULL,
+  `fecha` date NOT NULL,
+  `estado` enum('En Distribucion','Entregado') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `roles`
+--
+
+CREATE TABLE `roles` (
+  `id_rol` int(10) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `descripcion` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `roles`
+--
+
+INSERT INTO `roles` (`id_rol`, `nombre`, `descripcion`) VALUES
+(1, 'Cliente', 'Vista Mis Pedidos'),
+(2, 'Administrador', 'Vista de todo el panel de control');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `transporte`
+--
+
+CREATE TABLE `transporte` (
+  `id_transporte` varchar(50) NOT NULL,
+  `placa` varchar(6) NOT NULL,
+  `nombre_chofer` varchar(50) NOT NULL,
+  `apellidos_chofer` varchar(50) NOT NULL,
+  `cedula_chofer` char(10) NOT NULL,
+  `telefono` int(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id_usuario` int(10) NOT NULL,
+  `username` varchar(70) NOT NULL,
+  `nombres` varchar(50) NOT NULL,
+  `apellidos` varchar(50) NOT NULL,
+  `cedula_ciudadania` char(10) NOT NULL,
+  `telefono` int(10) NOT NULL,
+  `correo` varchar(50) DEFAULT NULL,
+  `direccion` varchar(50) DEFAULT NULL,
+  `contraseña` varchar(50) NOT NULL,
+  `rol` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id_usuario`, `username`, `nombres`, `apellidos`, `cedula_ciudadania`, `telefono`, `correo`, `direccion`, `contraseña`, `rol`) VALUES
+(7, 'admin', 'Jaydeve', 'Bernal Guisao', '1000131979', 316712445, 'jaydevemiranda2@gmail.com', 'calle 71', 'e00cf25ad42683b3df678c61f42c6bda', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `venta`
+--
+
+CREATE TABLE `venta` (
+  `id_venta` int(10) NOT NULL,
+  `id_cliente` int(10) NOT NULL,
+  `id_producto` int(11) NOT NULL,
+  `fecha` date NOT NULL,
+  `precio_unitario` int(20) NOT NULL,
+  `cantidad` int(10) NOT NULL,
+  `precio_total` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `categorias`
+--
+ALTER TABLE `categorias`
+  ADD PRIMARY KEY (`id_categoria`);
+
+--
+-- Indices de la tabla `compra`
+--
+ALTER TABLE `compra`
+  ADD PRIMARY KEY (`id_compra`),
+  ADD KEY `id_proveedor` (`id_proveedor`),
+  ADD KEY `id_producto` (`id_producto`);
+
+--
+-- Indices de la tabla `distribucion`
+--
+ALTER TABLE `distribucion`
+  ADD PRIMARY KEY (`id_distribucion`),
+  ADD KEY `id_venta` (`id_venta`),
+  ADD KEY `id_transporte` (`id_transporte`),
+  ADD KEY `numero_factura` (`numero_factura`);
+
+--
+-- Indices de la tabla `entrega`
+--
+ALTER TABLE `entrega`
+  ADD PRIMARY KEY (`id_entrega`),
+  ADD KEY `id_compra` (`id_compra`),
+  ADD KEY `transporte` (`transporte`),
+  ADD KEY `numero_factura` (`numero_factura`);
+
+--
+-- Indices de la tabla `facturas`
+--
+ALTER TABLE `facturas`
+  ADD PRIMARY KEY (`id_factura`),
+  ADD KEY `id_venta` (`id_venta`),
+  ADD KEY `id_compra` (`id_compra`);
+
+--
+-- Indices de la tabla `pedido`
+--
+ALTER TABLE `pedido`
+  ADD PRIMARY KEY (`id_pedido`),
+  ADD KEY `id_proveedor` (`id_proveedor`),
+  ADD KEY `clienteid_fk2` (`id_cliente`);
+
+--
+-- Indices de la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD PRIMARY KEY (`id_producto`),
+  ADD KEY `categoria` (`categoria`);
+
+--
+-- Indices de la tabla `proveedor`
+--
+ALTER TABLE `proveedor`
+  ADD PRIMARY KEY (`id_proveedor`);
+
+--
+-- Indices de la tabla `registro_pedido`
+--
+ALTER TABLE `registro_pedido`
+  ADD PRIMARY KEY (`id_registro_pedido`),
+  ADD KEY `id_pedido` (`id_pedido`);
+
+--
+-- Indices de la tabla `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id_rol`);
+
+--
+-- Indices de la tabla `transporte`
+--
+ALTER TABLE `transporte`
+  ADD PRIMARY KEY (`id_transporte`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id_usuario`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD KEY `rol_fgk` (`rol`);
+
+--
+-- Indices de la tabla `venta`
+--
+ALTER TABLE `venta`
+  ADD PRIMARY KEY (`id_venta`),
+  ADD KEY `id_producto` (`id_producto`),
+  ADD KEY `clienteid_fk1` (`id_cliente`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `categorias`
+--
+ALTER TABLE `categorias`
+  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `compra`
+--
+ALTER TABLE `compra`
+  MODIFY `id_proveedor` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `pedido`
+--
+ALTER TABLE `pedido`
+  MODIFY `id_proveedor` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `producto`
+--
+ALTER TABLE `producto`
+  MODIFY `categoria` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `proveedor`
+--
+ALTER TABLE `proveedor`
+  MODIFY `id_proveedor` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `registro_pedido`
+--
+ALTER TABLE `registro_pedido`
+  MODIFY `id_registro_pedido` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id_rol` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id_usuario` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `compra`
+--
+ALTER TABLE `compra`
+  ADD CONSTRAINT `compra_ibfk_1` FOREIGN KEY (`id_proveedor`) REFERENCES `proveedor` (`id_proveedor`),
+  ADD CONSTRAINT `compra_ibfk_2` FOREIGN KEY (`id_producto`) REFERENCES `producto` (`id_producto`);
+
+--
+-- Filtros para la tabla `distribucion`
+--
+ALTER TABLE `distribucion`
+  ADD CONSTRAINT `distribucion_ibfk_1` FOREIGN KEY (`id_venta`) REFERENCES `venta` (`id_venta`),
+  ADD CONSTRAINT `distribucion_ibfk_2` FOREIGN KEY (`id_transporte`) REFERENCES `transporte` (`id_transporte`),
+  ADD CONSTRAINT `distribucion_ibfk_3` FOREIGN KEY (`numero_factura`) REFERENCES `facturas` (`id_factura`);
+
+--
+-- Filtros para la tabla `entrega`
+--
+ALTER TABLE `entrega`
+  ADD CONSTRAINT `entrega_ibfk_1` FOREIGN KEY (`id_compra`) REFERENCES `compra` (`id_compra`),
+  ADD CONSTRAINT `entrega_ibfk_2` FOREIGN KEY (`transporte`) REFERENCES `transporte` (`id_transporte`),
+  ADD CONSTRAINT `entrega_ibfk_3` FOREIGN KEY (`numero_factura`) REFERENCES `facturas` (`id_factura`);
+
+--
+-- Filtros para la tabla `facturas`
+--
+ALTER TABLE `facturas`
+  ADD CONSTRAINT `facturas_ibfk_1` FOREIGN KEY (`id_venta`) REFERENCES `venta` (`id_venta`),
+  ADD CONSTRAINT `facturas_ibfk_2` FOREIGN KEY (`id_compra`) REFERENCES `compra` (`id_compra`);
+
+--
+-- Filtros para la tabla `pedido`
+--
+ALTER TABLE `pedido`
+  ADD CONSTRAINT `pedido_ibfk_2` FOREIGN KEY (`id_proveedor`) REFERENCES `proveedor` (`id_proveedor`);
+
+--
+-- Filtros para la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD CONSTRAINT `producto_ibfk_1` FOREIGN KEY (`categoria`) REFERENCES `categorias` (`id_categoria`);
+
+--
+-- Filtros para la tabla `registro_pedido`
+--
+ALTER TABLE `registro_pedido`
+  ADD CONSTRAINT `registro_pedido_ibfk_1` FOREIGN KEY (`id_pedido`) REFERENCES `pedido` (`id_pedido`);
+
+--
+-- Filtros para la tabla `venta`
+--
+ALTER TABLE `venta`
+  ADD CONSTRAINT `venta_ibfk_2` FOREIGN KEY (`id_producto`) REFERENCES `producto` (`id_producto`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
